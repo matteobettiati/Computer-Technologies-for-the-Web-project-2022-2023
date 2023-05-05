e2f2251d60dd78fde1c21aaa3d345e447af0b6c6
DenSandu <denisgabriel.sanduleanu@mail.polimi.it>
2023-05-05 13:16:58 +0200

    GoToPlaylistPage and homepage with all dynamic playlists

---
+++ src/main/java/it/polimi/tiw/controller/GoToPlaylistPage.java
@@ -0,0 +1,84 @@
+
+package it.polimi.tiw.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.beans.Playlist;
+import it.polimi.tiw.beans.Songs;
+import it.polimi.tiw.beans.User;
+import it.polimi.tiw.dao.PlaylistDAO;
+import it.polimi.tiw.dao.SongDAO;
+
+@WebServlet("/GoToPlayListPage")
+public class GoToPlaylistPage extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	private TemplateEngine templateEngine;
+
+	public void init() {
+		ServletContext context = getServletContext();
+
+		// Initializing the template engine
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(context);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+
+		try {
+
+			// Initializing the connection
+			String driver = context.getInitParameter("dbDriver");
+			String url = context.getInitParameter("dbUrl");
+			String user = context.getInitParameter("dbUser");
+			String password = context.getInitParameter("dbPassword");
+
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GoToPlaylistPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String playlistId = request.getParameter("idPlaylist");
+		response.getWriter().append(playlistId);
+
+	}
+
+}
