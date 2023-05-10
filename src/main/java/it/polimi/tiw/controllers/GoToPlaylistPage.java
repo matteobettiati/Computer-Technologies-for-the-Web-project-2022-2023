beeabf1193429d9393cfda6c40adff69a38722bd
matteobettiati <matteo.bet.38@gmail.com>
2023-05-10 12:56:20 +0200

    fixing

--- src/main/java/it/polimi/tiw/controllers/GoToPlaylistPage.java
+++ src/main/java/it/polimi/tiw/controllers/GoToPlaylistPage.java
@@ -4,7 +4,6 @@ package it.polimi.tiw.controllers;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
